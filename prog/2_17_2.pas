d817f0c1d437ce25d1962d53cb000ed8ec82430e
KeplyDog <tavarminii@gmail.com>
2024-10-12 17:22:28 +1000

    Add files via upload

---
+++ prog/2_17_2.pas
@@ -0,0 +1,23 @@
+﻿program idz2_17_2;
+
+var
+  a, b, c1, c2, c3, c4, c5 : real;
+  f : text;
+  
+begin
+  assign(f, 'C:\idz\prog\in2.dat');
+  reset(f);
+  
+  readln(f, a);
+  writeln('a = ', a);
+  
+  b := a*a*a*a*a; // За 5 операций
+  writeln('a^5 = ', b);
+  
+  c1 := a*a; // 1 + 1 = 2
+  c2 := c1*c1; // 2 + 2 = 4
+  c3 := c2*c2; // 4 + 4 = 8
+  c4 := c3*c3*c1*a; // 8 + 8 + 2 + 1 = 19
+  write('a^19 = ', c4); // За 6 операций
+  
+end.
---
+++ prog/2_17_3.pas
@@ -0,0 +1,32 @@
+﻿program idz2_17_3;
+
+var
+  c, d, x1, x2, res, n1, n11, n22, n2, n3 : real;
+  f : text;
+
+begin
+  assign(f, 'C:\idz\prog\in3.dat');
+  reset(f);
+  
+  readln(f, c, d);
+  close(f);
+  writeln(c, ' ', d);
+  
+  x1 := c - d;
+  x2 := c + d;
+  
+  n11 := sin(abs(c*x1*x1*x1 + d*x2*x2 - c*d));
+  n1 := n11*n11*n11;
+  n22 := c*x1*x1*x1 + d*x2*x2 - x1;
+  n2 := exp(0.5 * log(n22*n22 + pi));
+  n3 := tan(n22);
+  res := abs(n1/n2) + n3; 
+  
+  print(res);
+  
+  rewrite(f, 'C:\idz\prog\out3.dat');
+  
+  write(f, res);
+  
+  close(f);
+end.
